03961d187ad78b8e4445586870dc0480fe31c48f
zdeley <drey.santos2005@gmail.com>
2024-03-04 21:39:27 -0300

    inicializando exercicos em java. Fatec da Zona Leste

---
+++ src/Exercicio01.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+
+        double salario, novoSalario;
+
+        System.out.print("Digite o seu salário: ");
+        salario = entrada.nextDouble();
+
+        novoSalario = ((salario*25)/100) + salario;
+
+        System.out.print("Seu sálario terá um aumento de 25%!\n Seu sálario será de: " + novoSalario);
+
+    }
+}
