af181aedd5e256820df26482e3a636a3a1a767a0
zdeley <drey.santos2005@gmail.com>
2024-03-05 20:17:35 -0300

    continuacao exercicos em java. Fatec da Zona Leste

--- src/Exercicio04.java
+++ src/Exercicio04.java
@@ -6,7 +6,15 @@ public class Exercicio04 {
 
         System.out.print("Digite a quantos KG o saco de reção possui: ");
         sacoRacao = entrada.nextDouble();
-        System.out.print("Digite a quantidade de ração que voce dará para os gatos: ");
+        System.out.print("Digite a quantidade de ração em gramas que voce dará para os gatos: ");
+        qtdRacao = entrada.nextDouble();
 
+        sacoRacao = sacoRacao * 1000;
+        qtdRacao = (qtdRacao * 2) * 5;
+        restante = (sacoRacao - qtdRacao) / 1000;
+
+
+
+        System.out.print("Após 5 dias restará " + restante + " KG no saco de ração");
     }
 }
